acebd1bad1d64d301f8eca3ad1cf525790997ae9
Otis <altispec@gmail.com>
2015-10-21 16:08:23 +0800

    Fix registrations spec to use factory girl

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    name 'LETMEIN'
     email 'letmein@gmail.com'
     password '12345678'
     password_confirmation '12345678'
--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -1,30 +1,23 @@
 RSpec.describe '/api/v1/registrations' do
   describe 'POST' do
-    context 'when register user' do
-      let(:user_attributes) {
-        {
-          name: 'LETMEIN',
-          email: 'letmein@gmail.com',
-          password: '12345678',
-          password_confirmation: '12345678'
-        }
-      }
-
+    context 'when user register' do
+      let(:user_attributes) { attributes_for(:user) }
       it 'returns user info' do
-        post '/api/v1/registrations', attributes_for(:user)
+        post '/api/v1/registrations', user_attributes
 
         expect(User.count).to eq(1)
 
         user_json = json_body['user']
-        expect(user_json.name).to eq(user_attributes.name)
-        expect(user_json.email).to eq(user_attributes.email)
+        expect(user_json['name']).to eq(user_attributes[:name])
+        expect(user_json['email']).to eq(user_attributes[:email])
         expect(response.status).to eq(201)
       end
     end
-    context 'when register user with invalid value' do
+
+    context 'when user register with invalid value' do
       it 'return error messages' do
         post '/api/v1/registrations', attributes_for(:user, :invalid)
-        expect(json_body.fetch("errors")).not_to be_empty
+        expect(json_body.fetch('errors')).not_to be_empty
         expect(response.status).to eq 422
       end
     end
