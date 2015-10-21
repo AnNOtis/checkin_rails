5debcd686ddfadefddcdfdc49fc98550bc5269e5
Otis <altispec@gmail.com>
2015-10-21 23:23:25 +0800

    Add checkins request spec

---
+++ spec/requests/api/v1/checkins_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe '/api/v1/checkins' do
+  describe 'GET /api/v1/checkins' do
+    it 'returns a list of all checkins' do
+      create(:checkins)
+      create(:checkins, name: '另一個打卡')
+      get '/api/v1/checkins'
+
+      expect(json_body['checkins'].count).to eq(2)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'POST /api/v1/checkins' do
+    it 'creates the checkins' do
+      checkin_attributes = attributes_for(:checkins)
+      expect do
+        post '/api/v1/checkins', checkin_attributes
+      end.to change{ Checkin.count }.by(1)
+
+      expect(json_body['checkin']).to eq(checkin_attributes)
+      expect(response.status).to eq(201)
+    end
+  end
+
+  describe 'PUT /api/v1/checkins/:id' do
+    it 'updates the checkins' do
+      checkin = create(:checkin)
+      update_attributes = { name: '更改地點名稱' }
+      put '/api/v1/checkins', { id: checkin.id }.merge(update_attributes)
+
+      expect(json_body['checkin']).to eq(checkin.attributes.merge(update_attributes))
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'DELETE /api/v1/checkins/:id' do
+    it 'destroy the checkins' do
+      checkin = create(:checkin)
+      expect do
+        delete '/api/v1/checkins', id: checkin.id
+      end.to change{ Checkin.count }.by(1)
+
+      expect(response.status).to eq(204)
+    end
+  end
+end
