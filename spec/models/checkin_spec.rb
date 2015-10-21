922a0877e59ecb75281d3fe2fb8b4d4fbf8436f1
Otis <altispec@gmail.com>
2015-10-21 23:28:18 +0800

    Add unit test of checkin model with validations

---
+++ spec/models/checkin_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe Checkin do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:latitude) }
+    it { is_expected.to validate_presence_of(:longitude) }
+  end
+end
