cd67202128d025d757cd01ad97eae69350f494bf
Otis <altispec@gmail.com>
2015-10-22 00:08:46 +0800

    Add test case that auto-fetch address after checkin save

--- spec/models/checkin_spec.rb
+++ spec/models/checkin_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Checkin do
     it { is_expected.to validate_presence_of(:latitude) }
     it { is_expected.to validate_presence_of(:longitude) }
   end
+
+  context 'when create checkin' do
+    it 'saves address value automatically' do
+      checkin = create(:checkin, latitude: 40.7143528, longitude: -74.0059731)
+
+      expect(checkin.address).to eq('New York, NY, USA')
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,3 +57,19 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Geocoder.configure(:lookup => :test)
+
+Geocoder::Lookup::Test.set_default_stub(
+  [
+    {
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
