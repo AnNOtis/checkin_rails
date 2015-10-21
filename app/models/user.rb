899efb9f5a6d1632a20c2df27313601e46a99855
Otis <altispec@gmail.com>
2015-10-21 16:34:30 +0800

    Add user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # default scope
+
+  # constant
+
+  # attr macros
+
+  # association macros
+
+  # validation macros
+  validates :name, presence: true, uniqueness: true
+
+  # callback macros
+
+  # other macros
+  # =>  Include default devise modules. Others available are:
+  # =>  :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # scope macros
 end
