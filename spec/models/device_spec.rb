c75f2f7a87bc8168036653357890f68060bc9aa4
Otis <altispec@gmail.com>
2015-10-21 18:59:51 +0800

    Remove validate presence of user_id from device

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 RSpec.describe Device do
-  it { is_expected.to validate_presence_of(:user_id) }
   it { is_expected.to validate_presence_of(:device_token) }
   it { is_expected.to validate_uniqueness_of(:device_token).scoped_to(:user_id) }
 end
