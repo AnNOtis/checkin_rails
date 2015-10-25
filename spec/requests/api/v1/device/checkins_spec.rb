5b54d96a3f14f13a79330cc6e881c7c3732c60d1
Otis <altispec@gmail.com>
2015-10-25 17:50:33 +0800

    Add request spec for removing api list checkins from authorization

--- spec/requests/api/v1/device/checkins_spec.rb
+++ spec/requests/api/v1/device/checkins_spec.rb
@@ -3,36 +3,6 @@ require 'rails_helper'
 RSpec.describe '/api/v1/device/checkins' do
   let(:device) { create(:device) }
 
-  describe 'GET /api/v1/device/checkins' do
-    it 'returns a list of all checkins' do
-      create(:checkin)
-      create(:checkin, name: '另一個打卡')
-      get '/api/v1/device/checkins', device_token: device.device_token
-
-      expect(json_body['checkins'].count).to eq(2)
-      expect(response.status).to eq(200)
-    end
-
-    context 'when query near checkins' do
-      let(:current_lat){ 25.0477505 }
-      let(:current_lng){ 121.5148712 }
-
-      before do
-        create(:checkin, latitude: 25.047099, longitude: 121.516974) #64m
-        create(:checkin, latitude: 24.718455, longitude: 121.360017) #39.91km
-        create(:checkin, latitude: 23.624387, longitude: 120.682441) #179.11km
-      end
-
-      it 'return a list of near checkins in radius' do
-        get '/api/v1/device/checkins', \
-          device_token: device.device_token, lat: current_lat, lng: current_lng, radius: 50
-
-        expect(json_body['checkins'].count).to eq(2)
-        expect(response.status).to eq(200)
-      end
-    end
-  end
-
   describe 'POST /api/v1/device/checkins' do
     let(:checkin_attributes){ attributes_for(:checkin) }
 
